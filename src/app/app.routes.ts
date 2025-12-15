855c15c8120e7735311f4e3278460c262c817c1e
Naeomi Shane Dias <naeomishanedias@Naeomis-MacBook-Air.local>
2025-12-15 16:45:21 +0530

    Initial Angular setup with brand and admin dashboard modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { OverviewComponent } from './features/brand-dashboard/pages/overview/overview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'brand', component: OverviewComponent },
+    { path: '', redirectTo: 'brand', pathMatch: 'full' },
+    { path: '**', redirectTo: 'brand' }
+];
